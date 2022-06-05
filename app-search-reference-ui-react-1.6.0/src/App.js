14b5d0af4ee0b3496ae89645ce7575dc004da200
Thomas Hervey <thomasahervey@gmail.com>
2022-06-05 10:34:02 -0400

    moved: elasticsearch-js into separate foler; added: test_movies.ts

--- app-search-reference-ui-react-1.6.0/src/App.js
+++ app-search-reference-ui-react-1.6.0/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 
-// import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-
 import ElasticsearchAPIConnector from "@elastic/search-ui-elasticsearch-connector";
 
-import { exampleMovieAccess } from '../../key'
-
+import { exampleMovieAccess } from './key'
 
 import {
   ErrorBoundary,
@@ -21,34 +18,6 @@ import {
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
-// import {
-//   buildAutocompleteQueryConfig,
-//   buildFacetConfigFromConfig,
-//   buildSearchOptionsFromConfig,
-//   buildSortOptionsFromConfig,
-//   getConfig,
-//   getFacetFields
-// } from "./config/config-helper";
-
-// const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
-// const connector = new AppSearchAPIConnector({
-//   searchKey,
-//   engineName,
-//   hostIdentifier,
-//   endpointBase
-// });
-
-
-// const config = {
-//   searchQuery: {
-//     facets: buildFacetConfigFromConfig(),
-//     ...buildSearchOptionsFromConfig()
-//   },
-//   autocompleteQuery: buildAutocompleteQueryConfig(),
-//   apiConnector: connector,
-//   alwaysSearchOnInitialLoad: true
-// };
 
 
 const connector = new ElasticsearchAPIConnector({
@@ -185,42 +154,6 @@ export default function App() {
                 />
               </ErrorBoundary>
             </div>
-
-            // <div className="App">
-            //   <ErrorBoundary>
-            //     <Layout
-            //       header={<SearchBox autocompleteSuggestions={true} />}
-            //       sideContent={
-            //         <div>
-            //           {wasSearched && (
-            //             <Sorting
-            //               label={"Sort by"}
-            //               sortOptions={buildSortOptionsFromConfig()}
-            //             />
-            //           )}
-            //           {getFacetFields().map(field => (
-            //             <Facet key={field} field={field} label={field} />
-            //           ))}
-            //         </div>
-            //       }
-            //       bodyContent={
-            //         <Results
-            //           titleField={getConfig().titleField}
-            //           urlField={getConfig().urlField}
-            //           thumbnailField={getConfig().thumbnailField}
-            //           shouldTrackClickThrough={true}
-            //         />
-            //       }
-            //       bodyHeader={
-            //         <React.Fragment>
-            //           {wasSearched && <PagingInfo />}
-            //           {wasSearched && <ResultsPerPage />}
-            //         </React.Fragment>
-            //       }
-            //       bodyFooter={<Paging />}
-            //     />
-            //   </ErrorBoundary>
-            // </div>
           );
         }}
       </WithSearch>
---
+++ elasticsearch-js/src/test_movies.ts
@@ -0,0 +1,63 @@
+import fs from 'fs'
+import { Client } from '@elastic/elasticsearch'
+
+
+const config = {
+  node: 'https://localhost:9200',
+  auth: {
+    username: 'elastic',
+    password: 'example'
+  },
+  tls: {
+    ca: fs.readFileSync('./certs/es01/es01.crt'),
+    rejectUnauthorized: false
+  }
+}
+
+const client = new Client(config)
+
+
+async function runQuickstart () {
+  // // Let's start by indexing some data
+  // await client.index({
+  //   index: 'game-of-thrones',
+  //   document: {
+  //     character: 'Ned Stark',
+  //     quote: 'Winter is coming.'
+  //   }
+  // })
+
+  // await client.index({
+  //   index: 'game-of-thrones',
+  //   document: {
+  //     character: 'Daenerys Targaryen',
+  //     quote: 'I am the blood of the dragon.'
+  //   }
+  // })
+
+  // await client.index({
+  //   index: 'game-of-thrones',
+  //   document: {
+  //     character: 'Tyrion Lannister',
+  //     quote: 'A mind needs books like a sword needs a whetstone.'
+  //   }
+  // })
+
+  // here we are forcing an index refresh, otherwise we will not
+  // get any result in the consequent search
+  await client.indices.refresh({ index: 'my-example-movies' })
+
+  // Let's search!
+  const result= await client.search({
+    index: 'my-example-movies',
+    query: {
+      match_all: {}
+    }
+  })
+
+  console.log(result.hits.hits)
+
+}
+
+
+runQuickstart().catch(console.log)
