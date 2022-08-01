522eb43dd230840e31c1f22a466c37b137467d5c
Thomas Hervey <thomasahervey@gmail.com>
2022-08-01 15:49:10 -0400

    WIP

--- build/index.js
+++ build/index.js
@@ -62,7 +62,7 @@ function runQuickstart() {
         });
         console.log(result.hits.hits);
         // delete index once we're done
-        client.indices.delete({ index: 'game-of-thrones' });
+        // client.indices.delete({ index: 'game-of-thrones' })
     });
 }
 runQuickstart().catch(console.log);
